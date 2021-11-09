5f2b5abb9c6f91326a265e0698610c9eef3d55f2
Adrian Ramos <ramosayensa@outlook.com>
2021-11-09 16:57:34 +0100

    head del home

--- views/home.php
+++ views/home.php
@@ -3,38 +3,40 @@
 
 <head>
 
-
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../css/capelloStyle.css">
+    <title>Peluquería Capelos Belleza</title>
 </head>
 
 <body>
-<div id="encabezado">
-    <div id="menu">
-        <img src="../img/BELLEZAcapello-01.jpg" alt="logo">
-        <nav></nav>
+    <div id="encabezado">
+        <div id="menu">
+            <img src="../img/BELLEZAcapello-01.jpg" alt="logo">
+            <nav></nav>
+        </div>
+        <div id="info">
+
+        </div>
+        <img src="" alt="foto-carrusel">
+        <div id="cita"></div>
     </div>
-    <div id="info">
+    <div id="redes"></div>
+    <div id="tarifas"></div>
+    <div>
+        <p>Cancela tu cita con: 3 horas de antelación.</p>
+    </div>
+    <a class="nav-link" href="<?= "home" ?>">Usuarios</a>
+    <div id="newTrabajador">
+
+        <form id="formulario_trabajador_new" method="POST" action="../App.php?method=save_trabajador">
+            <label>Nombre: </label><input type="text" value="" name="nombre"><br>
+            <label>Apellidos: </label><input type="text" value="" name="apellidos"><br>
+            <label>Teléfono: </label><input type="text" value="" name="telefono"><br>
+
+            <input type="submit" value="enviar">
+        </form>
 
     </div>
-    <img src="" alt="foto-carrusel">
-    <div id="cita"></div>
-</div>
-<div id="redes"></div>
-<div id="tarifas"></div>
-<div>
-    <p>Cancela tu cita con: 3 horas de antelación.</p>
-</div>
-<a class="nav-link" href="<?= "home"?>">Usuarios</a>
-<div id="newTrabajador">
-
-    <form id="formulario_trabajador_new" method="POST" action="../App.php?method=save_trabajador">
-        <label>Nombre: </label><input type="text" value="" name="nombre"><br>
-        <label>Apellidos: </label><input type="text" value="" name="apellidos"><br>
-        <label>Teléfono: </label><input type="text" value="" name="telefono"><br>
-
-        <input type="submit" value="enviar">
-    </form>
-
-</div>
 
 
 

3bf7030b037fe8efd9a533478f350b102f9a3444
Adrian Ramos <30373697+AdrianRamosAyensa@users.noreply.github.com>
2021-11-11 18:49:43 +0100

    Metodos desde formularios con rutas bonitas

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -15,5 +15,21 @@ class LoginController
     {
         require "app/views/login.php";
     }
+
+    public function save_trabajador()
+    {
+        //echo "<h2> Hola mundo </h2>";
+
+
+        $arrParams = [];
+
+        if (isset($_POST['nombre'])) {
+            $nombreTrabajador =  $_POST['nombre'];
+            $arrParams['nombre'] = $nombreTrabajador;
+        }
+
+        //echo 'PEPEEEEEEE';
+        trabajador_new($arrParams);
+    }
 }
 

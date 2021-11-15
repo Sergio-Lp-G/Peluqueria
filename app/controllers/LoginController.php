46f9c15db3799ff17893895d300bc1cfde88cf6f
Adrian Ramos <30373697+AdrianRamosAyensa@users.noreply.github.com>
2021-11-15 19:54:53 +0100

    Conexion a base de datos y favicon

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -26,6 +26,43 @@ class LoginController
         if (isset($_POST['nombre'])) {
             $nombreTrabajador =  $_POST['nombre'];
             $arrParams['nombre'] = $nombreTrabajador;
+        } else {
+            $arrParams['nombre'] = "";
+        }
+
+        if (isset($_POST['apellidos'])) {
+            $apellidosTrabajador =  $_POST['apellidos'];
+            $arrParams['apellidos'] = $apellidosTrabajador;
+        } else {
+            $arrParams['apellidos'] = "";
+        }
+
+        if (isset($_POST['telefono'])) {
+            $telefonoTrabajador =  $_POST['telefono'];
+            $arrParams['telefono'] = $telefonoTrabajador;
+        } else {
+            $arrParams['telefono'] = 0;
+        }
+
+        if (isset($_POST['email'])) {
+            $emailTrabajador =  $_POST['email'];
+            $arrParams['email'] = $emailTrabajador;
+        } else {
+            $arrParams['email'] = "";
+        }
+
+        if (isset($_POST['login'])) {
+            $loginTrabajador =  $_POST['login'];
+            $arrParams['login'] = $loginTrabajador;
+        } else {
+            $arrParams['login'] = "";
+        }
+
+        if (isset($_POST['password'])) {
+            $passwordTrabajador =  $_POST['password'];
+            $arrParams['password'] = $passwordTrabajador;
+        } else {
+            $arrParams['password'] = "";
         }
 
         //echo 'PEPEEEEEEE';

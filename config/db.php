9e593a2df5cd585f2797012351f614bfd480d419
Sergio-Lp-G <sergiolpgar@gmail.com>
2021-11-16 16:27:37 +0000

    config

--- config/db.php
+++ config/db.php
@@ -2,6 +2,6 @@
 #config/db.php
 namespace Config;
 
-const DSN = 'mysql:dbname=capelos_belleza;host=db';
+const DSN = 'mysql:dbname=capelos;host=db';
 const USER = 'root';
 const PASSWORD = 'password';

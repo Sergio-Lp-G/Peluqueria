bc34d2e8464c222b3ae3a2a724f13288c5d1710f
Sergio-Lp-G <sergiolpgar@gmail.com>
2021-12-20 16:19:31 +0000

    redes css cambio

--- app/views/parts/redes.php
+++ app/views/parts/redes.php
@@ -1,7 +1,7 @@
 <div id="redes">
     <p id="tlf">-012345678-</p>
     <img src="/img/icono/rss.jpg" alt="rss">Enterate de las novedades (RSS feed)</br>
-    <img id="tw" src="/img/icono/twitter.jpg" alt="Twitter">Twitter
-    <img id="fcb" src="/img/icono/facebook.jpg" alt="Facebook">Facebook
-    <img id="flc" src="/img/icono/flickr.jpg" alt="Flickr">Flickr</br>
+    <img class="social" id="tw" src="/img/icono/twitter.jpg" alt="Twitter">Twitter
+    <img class="social" id="fcb" src="/img/icono/facebook.jpg" alt="Facebook">Facebook
+    <img class="social" id="flc" src="/img/icono/flickr.jpg" alt="Flickr">Flickr</br>
 </div>
